975b761f2716514aca21199e3b719a0b9aaa2457
Vignesh D <vignesh.d9123@gmail.com>
2025-08-08 18:28:51 +0530

    Testing for logs

--- progress-tracker/src/index.ts
+++ progress-tracker/src/index.ts
@@ -1,73 +1,73 @@
-import {WebSocketServer, WebSocket} from 'ws'
+import { WebSocketServer, WebSocket } from 'ws'
 import RedisClient from 'ioredis'
 import dotenv from 'dotenv'
 import jwt from 'jsonwebtoken'
 dotenv.config()
 const wss = new WebSocketServer({ port: 9090 })
-const subscribers:Map<string,Set<WebSocket>> = new Map<string,Set<WebSocket>>();
+const subscribers: Map<string, Set<WebSocket>> = new Map<string, Set<WebSocket>>();
 const redis = new RedisClient(process.env.REDIS_URL!)
 
 redis.on('connect', () => console.log("CONNECTED TO REDIS DB"))
-redis.psubscribe('video-progress*', (err , content)=>{
-    if(err){
+redis.psubscribe('video-progress*', (err, content) => {
+    if (err) {
         console.log("error", err)
         return
     }
     console.log("SUBSCRIBED TO CHANNEL COUNT", content)
 })
 
-redis.on('pmessage', (_, channel, message)=>{
+redis.on('pmessage', (_, channel, message) => {
     console.log("MESSAGE", channel, message)
     const videoId = channel.split(":")?.[1]
-    if(!videoId) return
-    if(!subscribers.has(videoId)) return
+    if (!videoId) return
+    if (!subscribers.has(videoId)) return
     subscribers.get(videoId)?.forEach(ws => {
-        if(ws.readyState === WebSocket.OPEN){
-        const messageJson = JSON.parse(message.toString())
-        const msg = {
-            videoId,
-            progress: messageJson.progress,
-            status: messageJson.status
-        }
-        ws.send(JSON.stringify(msg))
+        if (ws.readyState === WebSocket.OPEN) {
+            const messageJson = JSON.parse(message.toString())
+            const msg = {
+                videoId,
+                progress: messageJson.progress,
+                status: messageJson.status
+            }
+            ws.send(JSON.stringify(msg))
         }
     })
 })
 
 
-wss.on('connection', (ws:WebSocket, req) => {
+wss.on('connection', (ws: WebSocket, req) => {
     try {
         const reqUrl = new URL(req?.url!, `http://${req.headers.host}`);
         const token = reqUrl.searchParams.get("token");
-    
+
         if (!token) {
-          ws.close(4001, "Token missing");
-          return;
+            ws.close(4001, "Token missing");
+            return;
         }
-    
+
         const decoded = jwt.verify(token, process.env.JWT_SECRET!);
         (ws as any).user = decoded; // TODO: find a better way 
 
-      } catch (err) {
+    } catch (err) {
         ws.close(4002, "Invalid token");
-      }
+    }
     ws.on('message', (message) => {
         try {
             const messageJson = JSON.parse(message.toString())
-            if(messageJson.type === "SUBSCRIBE"){
-                const {videoId} = messageJson
-                if(!subscribers.has(videoId)){
+            if (messageJson.type === "SUBSCRIBE") {
+                const { videoId } = messageJson
+                if (!subscribers.has(videoId)) {
                     subscribers.set(videoId, new Set())
                 }
                 subscribers.get(videoId)?.add(ws)
             }
 
-            if(messageJson.type === "UNSUBSCRIBE"){
-                const {videoId} = messageJson
-                if(subscribers.has(videoId)){
-                    if(!subscribers.get(videoId)?.has(ws)) return
+            if (messageJson.type === "UNSUBSCRIBE") {
+                const { videoId } = messageJson
+                if (subscribers.has(videoId)) {
+                    if (!subscribers.get(videoId)?.has(ws)) return
                     subscribers.get(videoId)?.delete(ws)
-                    if(subscribers.get(videoId)?.size === 0){
+                    if (subscribers.get(videoId)?.size === 0) {
                         subscribers.delete(videoId)
                     }
                 }
